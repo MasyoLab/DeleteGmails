37687abf54171f2b0c9e3c20dda6d15ae7b97dd3
masyo <masyo.lab@gmail.com>
2023-01-13 22:51:29 +0900

    Update and rename DeleteGmails.gs to DeleteGmails.js

--- DeleteGmails.js
+++ DeleteGmails.js
@@ -9,9 +9,11 @@ var SHEET_GID = null;
 var DISCORD_WEBHOOKS = null;
 var APP_DEBUG = false;
 
-/** gasのpost関数 */
-function doPost(e)
-{
+/**
+ * gasのpost関数
+ * @param {any} e
+ */
+function doPost(e) {
   APP_DEBUG = isNull(e);
 
   var sp = PropertiesService.getScriptProperties();
@@ -23,12 +25,10 @@ function doPost(e)
 }
 
 /** メインの処理 */
-function main()
-{
+function main() {
   var spreadSheet = SpreadsheetApp.openById(SHEET_ID);
   var sheet = spreadSheet.getSheets().find(v => v.getSheetId() == SHEET_GID);
-  if (isNull(sheet))
-  {
+  if (isNull(sheet)) {
     return;
   }
 
@@ -36,13 +36,11 @@ function main()
   var range = sheet.getDataRange();
   var values = range.getValues();
 
-  for (var i = 0; i < values.length; i++)
-  {
+  for (var i = 0; i < values.length; i++) {
     actionArray.push(values[i][0]);
   }
 
-  if (APP_DEBUG)
-  {
+  if (APP_DEBUG) {
     Logger.log(SHEET_ID);
     Logger.log(SHEET_GID);
     Logger.log(DISCORD_WEBHOOKS);
@@ -53,20 +51,20 @@ function main()
   startMessage();
 
   // 検索コマンドを指定する
-  for (var i = 0; i < actionArray.length; i++)
-  {
+  for (var i = 0; i < actionArray.length; i++) {
     deleteGmails(actionArray[i]);
   }
 
   endMessage();
 }
 
-/** 指定コマンドで検索したメールを削除する処理 */
-function deleteGmails(actionStr)
-{
+/**
+ * 指定コマンドで検索したメールを削除する処理
+ * @param {String} actionStr
+ */
+function deleteGmails(actionStr) {
   var deleteThreads = GmailApp.search(actionStr);
-  if (isNull(deleteThreads) || deleteThreads.length == 0)
-  {
+  if (isNull(deleteThreads) || deleteThreads.length == 0) {
     return;
   }
 
@@ -76,8 +74,7 @@ function deleteGmails(actionStr)
   postDiscordBOT(logStr);
 
   // 該当メールを全て削除
-  for (var i = 0; i < deleteThreads.length; i++)
-  {
+  for (var i = 0; i < deleteThreads.length; i++) {
     deleteThreads[i].moveToTrash();
   }
 
@@ -86,9 +83,11 @@ function deleteGmails(actionStr)
   postDiscordBOT(logStr);
 }
 
-/** Discord API を実行する */
-function postDiscordBOT(str)
-{
+/**
+ * Discord API を実行する
+ * @param {String} str
+ */
+function postDiscordBOT(str) {
   // 投稿するチャット内容と設定
   var message = {
     "content": str, // チャット本文
@@ -104,23 +103,23 @@ function postDiscordBOT(str)
   Utilities.sleep(SLEEP_TIME); // 0.3秒間の間に、連続でAPIを実行できないので待機処理
 }
 
-function startMessage()
-{
+function startMessage() {
   var logStr = STAMP_COFFEE + '\n';
   logStr += 'start DeleteGmails';
   postDiscordBOT(logStr);
 }
 
-function endMessage()
-{
+function endMessage() {
   var logStr = STAMP_COFFEE + '\n';
   logStr += 'end DeleteGmails\n';
   logStr += 'all done';
   postDiscordBOT(logStr);
 }
 
-/** null */
-function isNull(value)
-{
+/**
+ * null
+ * @param {any} value
+ */
+function isNull(value) {
   return value == null;
 }
